2d697d0475b1685f4d60f92b9351be32d41d8970
julien <julien.luccioni2014@gmail.com>
2019-11-03 21:34:49 +0100

    init project and config

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import http, { IncomingMessage, ServerResponse, Server } from "http";
+import fs from "fs";
+import path from "path";
+
+function serverHandler(req: IncomingMessage, res: ServerResponse): void {
+  const indexPath = path.join(__dirname, "view", "index.html");
+  fs.readFile(indexPath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("internal error");
+      return;
+    }
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+const server: Server = http.createServer(serverHandler);
+
+server.listen(8081);
+console.log("Server Listening");
